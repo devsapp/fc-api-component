7f1b516313dd2737131b6dd2505e7b2a0f9d71b7
wss-git <wssgryx@163.com>
2022-06-08 14:06:15 +0800

    fix delta aone

--- src/index.ts
+++ src/index.ts
@@ -113,7 +113,7 @@ export default class Component {
                     header: `Options`,
                     optionList: [{
                         name: 'region',
-                        description: `[Required] Specify the fc region, value: cn-hangzhou/cn-beijing/cn-hangzhou/cn-shanghai/cn-qingdao/cn-zhangjiakou/cn-huhehaote/cn-shenzhen/cn-chengdu/cn-hongkong/ap-southeast-1/ap-southeast-2/ap-southeast-3/ap-southeast-5/ap-northeast-1/eu-central-1/eu-west-1/us-west-1/us-east-1/ap-south-1\n* Set default region: [s cli fc-default set api-default-region <region>]`,
+                        description: `[Required] Specify the fc region, value: cn-hangzhou/cn-beijing/cn-shanghai/cn-qingdao/cn-zhangjiakou/cn-huhehaote/cn-shenzhen/cn-chengdu/cn-hongkong/ap-southeast-1/ap-southeast-2/ap-southeast-3/ap-southeast-5/ap-northeast-1/eu-central-1/eu-west-1/us-west-1/us-east-1/ap-south-1\n* Set default region: [s cli fc-default set api-default-region <region>]`,
                         type: String,
                     }, {
                         name: 'access',
