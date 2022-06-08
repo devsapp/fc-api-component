a08cfea02b93d1bb3a23aaf1daad0d80df90968d
wss-git <wssgryx@163.com>
2022-06-08 15:03:38 +0800

    build: 0.0.7

---
+++ dist/index.d.ts
@@ -0,0 +1,35 @@
+import { InputProps } from './common/entity';
+export default class Component {
+    private requestPath;
+    private apiContent;
+    private inputs;
+    private fcDefault;
+    private comParse;
+    private requestMethod;
+    private body;
+    private headers;
+    private query;
+    private requestRegion;
+    private bodyType;
+    constructor();
+    private getClient;
+    /**
+     * demo fc api 默认指令
+     * @param inputs
+     * @returns
+     */
+    index(inputs: InputProps): Promise<any>;
+    getRequestData(): Promise<void>;
+    /**
+     * demo fc api 20160815 版本
+     * @param inputs
+     * @returns
+     */
+    v20160815(): Promise<any>;
+    /**
+     * demo fc api 20210406 版本
+     * @param inputs
+     * @returns
+     */
+    v20210416(): Promise<any>;
+}
